b596acce5acb7527c739da4131798755ad2c0218
otitoju1 <john.o@allprimetech.com>
2022-09-19 00:59:08 +0100

    finish with login screen

---
+++ components/Button.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { Button as PaperButton } from 'react-native-paper'
+import { theme } from '../core/theme'
+
+export default function Button({ mode, style, ...props }) {
+  return (
+    <PaperButton
+      style={[
+        styles.button,
+        mode === 'outlined' && { backgroundColor: theme.colors.secondary },
+        style,
+      ]}
+      labelStyle={styles.text}
+      mode={mode}
+      {...props}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: '100%',
+    marginVertical: 10,
+    paddingVertical: 2,
+    backgroundColor: '#ea4c89',
+    borderRadius: 45
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 15,
+    lineHeight: 26,
+  },
+})
